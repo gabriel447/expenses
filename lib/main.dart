38e8979a34b1451d8d759408a697cf5b6883b740
Gabriel447 <gabriel_447@live.com>
2022-09-13 21:35:38 -0300

    button

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
       ),
       //componente que deixa a lista rolavel
       body: SingleChildScrollView(
@@ -35,6 +41,11 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
